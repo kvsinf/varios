517652439d497643ae4fb22c362bee19f067c215
kvsinf <90481625+kvsinf@users.noreply.github.com>
2026-03-26 11:20:43 -0400

    Update patron_SII.sql

--- sii_154/patron_SII.sql
+++ sii_154/patron_SII.sql
@@ -1,1 +1,13 @@
-
+SELECT *
+FROM patron_SII
+WHERE id_patronSII IN ('Patente','PatenteC','RUTTrans','RUTChofer','NombreChof','DirDest','CmnaDest','CiudadDest')
+ORDER BY CASE id_patronSII
+           WHEN 'Patente'   THEN 1
+           WHEN 'PatenteC'  THEN 2
+           WHEN 'RUTTrans'  THEN 3
+           WHEN 'RUTChofer' THEN 4
+           WHEN 'NombreChof' THEN 5
+           WHEN 'DirDest' THEN 6
+           WHEN 'CmnaDest' THEN 7
+           WHEN 'CiudadDest' THEN 8
+         END;
