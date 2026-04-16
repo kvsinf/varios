24e642123c999ff9347844d48fe7cb683600678b
kvsinf <90481625+kvsinf@users.noreply.github.com>
2026-04-16 19:40:44 -0400

    Update imap.sql

--- correo/imap.sql
+++ correo/imap.sql
@@ -15,3 +15,8 @@ WHERE
         'scopeimap',
         'carpeta_IMAP'
     );
+
+
+SELECT elem, valor 
+FROM configuracion 
+WHERE elem IN ('Server_IMAP', 'portIMAP','user_IMAP','password_IMAP','POP3/IMAP','tenantid','clientid','scopeimap','carpeta_IMAP');
