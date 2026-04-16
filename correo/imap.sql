57289e49c817d3be89e62a7ae8337cb265ebd15e
kvsinf <90481625+kvsinf@users.noreply.github.com>
2026-04-16 19:40:07 -0400

    Format SQL query for better readability

--- correo/imap.sql
+++ correo/imap.sql
@@ -1,3 +1,17 @@
-SELECT elem, valor 
-FROM configuracion 
-WHERE elem IN ('Server_IMAP', 'portIMAP','user_IMAP','password_IMAP','POP3/IMAP','tenantid','clientid','scopeimap','carpeta_IMAP');
+SELECT 
+    elem, 
+    valor
+FROM 
+    configuracion
+WHERE 
+    elem IN (
+        'Server_IMAP',
+        'portIMAP',
+        'user_IMAP',
+        'password_IMAP',
+        'POP3/IMAP',
+        'tenantid',
+        'clientid',
+        'scopeimap',
+        'carpeta_IMAP'
+    );
